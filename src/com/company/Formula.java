a5632052d4941f406d106b4fcb13a37724dbb4d9
Daria2313 <tapocheksuga2003@gmail.com>
2022-04-02 12:52:23 +0300

    commit

---
+++ src/com/company/Formula.java
@@ -0,0 +1,18 @@
+package com.company;
+
+public class Formula {
+    float x;
+    float y;
+    float z;
+
+    public Formula(float x, float y, float z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+
+    public void displayFormula() {
+        double l = Math.sqrt(x * x + y * y + z * z);
+        System.out.println(l);
+    }
+}
---
+++ src/com/company/Main.java
@@ -0,0 +1,19 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner num = new Scanner(System.in);
+        float x = num.nextFloat();
+        float y = num.nextFloat();
+        float z = num.nextFloat();
+        Formula did = new Formula(x,y,z);
+        did.displayFormula();
+    }
+}
+
+
+
+
